c302d2372422d0e92d4d8979fef3e1536ded575b
ColorCode <benjaaguilar02@gmail.com>
2025-04-29 20:19:46 -0300

    feat: add utils file and createJWT function

---
+++ src/lib/utils.ts
@@ -0,0 +1,29 @@
+import { User } from '../generated/prisma';
+import jsonwebtoken from 'jsonwebtoken';
+import { InternalError } from './customErrors';
+
+export function createJWT(user: User) {
+  const id = user.id;
+
+  const expiresIn = '1h';
+
+  const payload = {
+    sub: id,
+    iat: Math.floor(Date.now() / 1000),
+  };
+
+  const secret = process.env.SECRET_JWT;
+  if (!secret) {
+    throw new InternalError('SECRET_JWT is not defined on env variables', 500);
+  }
+
+  const signedToken = jsonwebtoken.sign(payload, secret, {
+    expiresIn: expiresIn,
+    algorithm: 'HS256',
+  });
+
+  return {
+    token: signedToken,
+    expires: expiresIn,
+  };
+}
