d62cafd9514558e28a683ddd96c9f850f028f20e
ColorCode <benjaaguilar02@gmail.com>
2025-04-29 16:01:41 -0300

    feat: use errorHandler middleware

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import router from './routes/indexRoute';
+import errorHandler from './controllers/errorHandler';
 
 const app = express();
 
@@ -7,6 +8,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.use('/', router);
+
+app.use(errorHandler);
 
 const PORT = 3000;
 app.listen(PORT, () => console.log(`server running on port ${PORT}`));
