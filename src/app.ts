d6d79e7e554802c1c4e6d3e8661f0281af1a369d
ColorCode <benjaaguilar02@gmail.com>
2025-04-29 20:18:38 -0300

    chore: add dotenv config

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,11 @@
 import express from 'express';
 import router from './routes/indexRoute';
 import errorHandler from './controllers/errorHandler';
+import dotenv from 'dotenv';
 
 const app = express();
 
+dotenv.config();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
