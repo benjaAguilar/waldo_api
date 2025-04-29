2812f98e2dd316275e8d37b67dbedca4d1543418
ColorCode <benjaaguilar02@gmail.com>
2025-04-29 16:02:19 -0300

    feat: add trycatch and steps comment

--- src/routes/indexRoute.ts
+++ src/routes/indexRoute.ts
@@ -1,8 +1,24 @@
-import { Router } from "express";
+import { Router } from 'express';
+import { createUser } from '../controllers/userController';
+import { tryCatch } from '../lib/tryCatch';
 const router = Router();
 
-router.get("/", (req, res, next) => {
-  res.json({ route: true });
-});
+/*
+  >> Al hacer play crear el usuario y enviar la cookie al navegador con la data de la imagen.
+          > crear usuario y enviar la cookie al navegador
+          > enviar datos de la imagen
+          > asociar usuario con imagen y leaderboard elegidos
+
+  >> Desde el front enviar la fecha de inicio cuando la imagen se cargo.
+          > guardar fecha en la base de datos
+
+  >> Desde el front se envia la fecha de finalizacion cuando se encontro a waldo.
+          > guardar fecha de finalizacion
+          > realizar calculo para obtener el tiempo en milisegundos
+          > enviar respuesta al front y esperar el nombre de retorno
+          > los datos se asociaran a la leaderboard
+*/
+
+router.post('/user', tryCatch(createUser));
 
 export default router;
