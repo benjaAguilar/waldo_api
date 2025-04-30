5f0e1f708f2b024700a3ee575281ea023debf352
ColorCode <benjaaguilar02@gmail.com>
2025-04-30 12:08:13 -0300

    feat: add passport middleware

--- src/app.ts
+++ src/app.ts
@@ -2,12 +2,19 @@ import express from 'express';
 import router from './routes/indexRoute';
 import errorHandler from './controllers/errorHandler';
 import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
+import passport from 'passport';
+import configurePassport from './config/passportConfig';
 
 const app = express();
 
 dotenv.config();
+app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+configurePassport(passport);
+app.use(passport.initialize());
 
 app.use('/', router);
 
