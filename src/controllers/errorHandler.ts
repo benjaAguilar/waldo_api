5f5ee0d167eacf85a3a23081211a93e4f3821d54
ColorCode <benjaaguilar02@gmail.com>
2025-04-30 17:21:31 -0300

    feat: add instance of errors validation

--- src/controllers/errorHandler.ts
+++ src/controllers/errorHandler.ts
@@ -1,9 +1,26 @@
-import { Request, Response, NextFunction, ErrorRequestHandler } from 'express';
+import { ErrorRequestHandler } from 'express';
+import { CustomError, DbError } from '../lib/customErrors';
 
 const errorHandler: ErrorRequestHandler = (err, req, res, next) => {
-  console.log(err);
-  console.log('----------');
-  console.log(err.message);
+  console.error(err);
+  console.error(err.message);
+
+  if (err instanceof CustomError) {
+    res.status(err.statusCode).json({
+      success: false,
+      message: err.message,
+    });
+    return;
+  }
+
+  if (err instanceof DbError) {
+    res.status(err.statusCode).json({
+      success: false,
+      message: err.message,
+      dbErr: err.dbErr,
+    });
+    return;
+  }
 
   res.status(500).json({ success: false, message: 'Internal Server Error' });
 };
