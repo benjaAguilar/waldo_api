412612311561a41de63a8e260cfa298b2c9f7e6b
ColorCode <benjaaguilar02@gmail.com>
2025-04-30 09:36:57 -0300

    feat: add addWaldoImage middleware

--- src/controllers/imageController.ts
+++ src/controllers/imageController.ts
@@ -1,5 +1,29 @@
 import { Request, Response, NextFunction } from 'express';
+import { addNewImage, addNewLeaderboard } from '../db/queries';
+import { ImageData, Leaderboard } from '@prisma/client';
 
-async function addWaldoImage(req: Request, res: Response, next: NextFunction) {
+export async function addWaldoImage(
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) {
   const { x1, x2, y1, y2, width, height, imgRoute, name } = req.body;
+
+  const image: ImageData = await addNewImage(
+    parseInt(x1),
+    parseInt(x2),
+    parseInt(y1),
+    parseInt(y2),
+    parseInt(width),
+    parseInt(height),
+    imgRoute,
+    name,
+  );
+
+  const leaderboard: Leaderboard = await addNewLeaderboard(
+    image.name,
+    image.id,
+  );
+
+  res.json({ image, leaderboard });
 }
