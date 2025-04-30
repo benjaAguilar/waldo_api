45edd0f8b96d6a91ec7ad56ee6eebd02c87e6530
ColorCode <benjaaguilar02@gmail.com>
2025-04-30 15:02:07 -0300

    feat: add node_env='test' for testing db

--- src/db/prismaClient.ts
+++ src/db/prismaClient.ts
@@ -1,2 +1,17 @@
 import { PrismaClient } from '@prisma/client';
-export const prisma = new PrismaClient();
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const databaseUrl =
+  process.env.NODE_ENV === 'test'
+    ? process.env.TEST_DATABASE_URL
+    : process.env.DATABASE_URL;
+
+export const prisma = new PrismaClient({
+  datasources: {
+    db: {
+      url: databaseUrl,
+    },
+  },
+});
