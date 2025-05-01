167ef924e56b37ee38152c97846fd060203ba376
ColorCode <benjaaguilar02@gmail.com>
2025-05-01 12:35:07 -0300

    feat: add passport and cookie config on testApp

--- src/tests/testApp.ts
+++ src/tests/testApp.ts
@@ -1,11 +1,23 @@
 import express from 'express';
 import router from '../routes/indexRoute';
 import errorHandler from '../controllers/errorHandler';
+import passport from 'passport';
+import configurePassport from '../config/passportConfig';
+import cookieParser from 'cookie-parser';
+import dotenv from 'dotenv';
 
 const app = express();
 
+dotenv.config();
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use(cookieParser());
+configurePassport(passport);
+app.use(passport.initialize());
+
 app.use('/', router);
+
 app.use(errorHandler);
 
 export default app;
