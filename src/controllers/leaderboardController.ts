8a9f29732753a7bbc83c2eeb80ef1b581319c2b5
ColorCode <benjaaguilar02@gmail.com>
2025-05-05 12:05:07 -0300

    feat: add leaderboardController

---
+++ src/controllers/leaderboardController.ts
@@ -0,0 +1,26 @@
+import { Request, Response, NextFunction } from 'express';
+import { getLeaderboardById, getUsersByLeaderboard } from '../db/queries';
+import { CustomError } from '../lib/customErrors';
+
+async function getLeaderboard(req: Request, res: Response, next: NextFunction) {
+  const leaderboardId = parseInt(req.params.id);
+
+  if (isNaN(leaderboardId)) {
+    return next(new CustomError('Provide a valid leaderboard param', 400));
+  }
+
+  const leaderboard = await getLeaderboardById(leaderboardId);
+  const users = await getUsersByLeaderboard(leaderboardId);
+
+  if (!leaderboard) {
+    return next(new CustomError('Leaderboard not found', 404));
+  }
+
+  res.json({
+    success: true,
+    leaderboard,
+    users,
+  });
+}
+
+export { getLeaderboard };
